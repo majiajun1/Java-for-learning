f5e0b21674a7f392f6e440ae8578edd911143c42
MaJiaJun1212 <1486902669@qq.com>
2024-10-20 19:52:55 +0800

    10.20 19:52

---
+++ JAVA SE/ForceTransform.java
@@ -0,0 +1,15 @@
+public class ForceTransform
+{
+    public static void main(String[] args)
+    {
+        int i1=123;
+        byte b=(byte)i1;
+        System.out.println(b);
+
+        FreshJuice fj=new FreshJuice();
+        FreshJuice fj1=new FreshJuice();
+        fj.test=1;
+        System.out.println(fj1.test);
+    }
+
+}
--- JAVA SE/FreshJuice.java
+++ JAVA SE/FreshJuice.java
@@ -1,5 +1,5 @@
 public class FreshJuice {
     enum ChineseJuiceSize{SMALL,MEDIUM,LARGE};
     ChineseJuiceSize size;
-
+    static int test;
 }
