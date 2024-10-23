4db06b249f7a846b7b2577eb29f67e0ab727ebcd
MaJiaJun1212 <1486902669@qq.com>
2024-10-23 20:59:08 +0800

    commit:202410232059

--- JAVA SE/DateDemo.java
+++ JAVA SE/DateDemo.java
@@ -1,5 +1,7 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.*;
+import java.text.*;
 public class DateDemo {
     public static void main(String[] args) {
         Date date = new Date();
@@ -16,5 +18,24 @@ public class DateDemo {
         System.out.println(sdf.format(newdate));
 
         System.out.printf("%tR", date);
+        System.out.println();
+        System.out.printf("%1$s %2$tB %2$td, %2$tY",
+                         "Due date:", date);  // 格式化字符串
+
+        System.out.println();
+        SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd");
+
+      String input = args.length == 0 ? "1818-11-11" : args[0];
+
+      System.out.print(input + " Parses as ");
+
+      Date t;
+
+      try {
+          t = ft.parse(input);
+          System.out.println(t);
+      } catch (ParseException e) {
+          System.out.println("Unparseable using " + ft);
+      }
     }
 }
