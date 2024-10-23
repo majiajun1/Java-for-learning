550d9b8e6c21fea49abfbe2f0c76f68d9518c36b
MaJiaJun1212 <1486902669@qq.com>
2024-10-23 20:41:52 +0800

    commit:202410232041

--- JAVA SE/DateDemo.java
+++ JAVA SE/DateDemo.java
@@ -1,3 +1,4 @@
+import java.text.SimpleDateFormat;
 import java.util.Date;
 public class DateDemo {
     public static void main(String[] args) {
@@ -8,6 +9,12 @@ public class DateDemo {
         System.out.println(b);
         b.insert(10,"sdjfhsdjkifh");
         System.out.println(b);
+        System.out.println(date.before(new Date()));
 
+        Date newdate=new Date();
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(sdf.format(newdate));
+
+        System.out.printf("%tR", date);
     }
 }
