ac6d1fc8c22ee5bc85d9db7511a90c07b8ca5e3d
MaJiaJun1212 <1486902669@qq.com>
2024-10-25 20:44:49 +0800

    commit:202410252044

---
+++ JAVA SE/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+    private String name;
+    private int id;
+    public Animal(String myname, int myid) {
+        name=myname;
+        id=myid;
+    }
+    public void eat()
+    {
+        System.out.println("Eating...");
+    }
+}
---
+++ JAVA SE/ChenglianglunTest.java
@@ -0,0 +1,7 @@
+public class ChenglianglunTest {
+    public static void main(String[] args) {
+        Chenglianglun nm=new Chenglianglun("abc",12);
+        nm.getWage();
+
+    }
+}
