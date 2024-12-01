1bbcfe50842a8f9fd0a39c4009a05f50feec677e
MaJiaJun1212 <1486902669@qq.com>
2024-12-01 23:31:49 +0800

    commit:202412012331

--- Java 多线程.md
+++ Java 多线程.md
@@ -845,5 +845,50 @@ blockingQueue不是新的东西
 
 put、take方法
 
+~~~java	
+
+public class SynchronousQueuelearn {
+    public static void main(String[] args) throws InterruptedException {
+
+        SynchronousQueue<String> blockingQueue = new SynchronousQueue<>();
+        //同步队列
+        //SynchronousQueue不存储元素
+        //put了一个元素 必须先take出来 否则不能再put进去值
+        new Thread(()->{
+
+
+            try{System.out.println(Thread.currentThread().getName()+" put 1");
+             blockingQueue.put("1");
+            System.out.println(Thread.currentThread().getName()+" put 2");
+             blockingQueue.put("2");
+            System.out.println(Thread.currentThread().getName()+" put 3");
+             blockingQueue.put("3");}
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        },"T1").start();
+
+
+        new Thread(()->{
+
+
+            try{
+                 TimeUnit.SECONDS.sleep(3);
+                 System.out.println(Thread.currentThread().getName()+blockingQueue.take());
+                  TimeUnit.SECONDS.sleep(3);
+                 System.out.println(Thread.currentThread().getName()+blockingQueue.take());
+                  TimeUnit.SECONDS.sleep(3);
+                 System.out.println(Thread.currentThread().getName()+blockingQueue.take());
+
+             }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        },"T2").start();
+    }
+}
+
+~~~
+
 
 
--- JavaThreadLearning/SynchronousQueuelearn.java
+++ JavaThreadLearning/SynchronousQueuelearn.java
@@ -1,9 +1,43 @@
+import java.sql.Time;
 import java.util.concurrent.*;
 
 public class SynchronousQueuelearn {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        SynchronousQueue<String> blockingQueue = new SynchronousQueue<>();
+        //同步队列
+        //SynchronousQueue不存储元素
+        //put了一个元素 必须先take出来 否则不能再put进去值
+        new Thread(()->{
 
 
+            try{System.out.println(Thread.currentThread().getName()+" put 1");
+             blockingQueue.put("1");
+            System.out.println(Thread.currentThread().getName()+" put 2");
+             blockingQueue.put("2");
+            System.out.println(Thread.currentThread().getName()+" put 3");
+             blockingQueue.put("3");}
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        },"T1").start();
 
+
+        new Thread(()->{
+
+
+            try{
+                 TimeUnit.SECONDS.sleep(3);
+                 System.out.println(Thread.currentThread().getName()+blockingQueue.take());
+                  TimeUnit.SECONDS.sleep(3);
+                 System.out.println(Thread.currentThread().getName()+blockingQueue.take());
+                  TimeUnit.SECONDS.sleep(3);
+                 System.out.println(Thread.currentThread().getName()+blockingQueue.take());
+
+             }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        },"T2").start();
     }
 }
