7585f63f14210cf22ad94e0888cd2feb00676ee7
MaJiaJun1212 <1486902669@qq.com>
2025-02-28 17:02:59 +0800

    commit:202502281702

--- Leetcode/FindThePeakErShua162.java
+++ Leetcode/FindThePeakErShua162.java
@@ -1,5 +1,32 @@
-public class FindThePeakErShua162 {
-     public int findPeakElement(int[] nums) {
+import java.util.ArrayDeque;
 
+public class FindThePeakErShua162 {
+//     public int findPeakElement(int[] nums) {
+//         ArrayDeque<Integer> stack = new ArrayDeque<>();
+//         stack.push(0);
+//         for(int i = 1; i < nums.length; i++){
+//             if(nums[i] > nums[stack.peek()]){
+//                 stack.push(i);
+//             }
+//         }
+//         return stack.peek();
+//
+//    }
+
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        int left = 0, right = n-1;
+        int mid = (left + right)/2;
+        while(left < right){
+            mid = (left + right)/2;
+            if(nums[mid] < nums[mid+1]){
+                left = mid+1;
+            }else
+            {
+                right = mid;
+            }
+        }
+        return left;
     }
+
 }
