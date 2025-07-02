fc00df6035bc6ce7af9fe8afd944e115f89e77f5
MaJiaJun1212 <1486902669@qq.com>
2025-07-02 23:54:53 +0800

    commit:202507022

--- LeetcodeErShua/com/HeBing88.java
+++ LeetcodeErShua/com/HeBing88.java
@@ -10,15 +10,44 @@ import java.util.Arrays;
 
 public class HeBing88 {
     public static void main(String[] args) {
-        int[] arr = { 1,2,4,5,6,0 };
-        int m=5;
-        int[] nums2={3};
-        int n=1;
+        int[] arr = {0,0,0,0,0};
+        int m=0;
+        int[] nums2={1,2,3,4,5};
+        int n=5;
         merge(arr,m,nums2,n);
+        for (int i : arr  ) {
+            System.out.println(i);
+        }
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-         //合并再排序
+        if(Math.min(m,n)==0)
+        {
+            if(m>n)
+            {
+                return;
+            }else
+            {
+                for(int i=0;i<m+n;i++)
+                {
+                    nums1[i]=nums2[i];
+
+
+                }
+                 Arrays.sort(nums1);
+                return;
+            }
+        }
+
+
+            int index=m;
+            int i=0;
+            while(index<m+n)
+            {
+                nums1[index++]=nums2[i++];
+            }
+            Arrays.sort(nums1);
+            return;
 
     }
 
